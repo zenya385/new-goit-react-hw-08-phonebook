6cacb5e3c9d1e476ba09dc18164a146c1aeddce5
Roman Zeno <zeno385@gmail.com>
2022-02-06 15:38:24 +0200

    deploy

--- src/App.js
+++ src/App.js
@@ -11,7 +11,6 @@ class App extends Component {
       { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
       { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
       { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-      { id: 'id-5', name: 'Ann Copel', number: '227-26' },
     ],
     filter: '',
   };
