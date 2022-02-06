9303e22188f9d7d6eb4c4c43c423118b1a6e71f6
Roman Zeno <zeno385@gmail.com>
2022-02-06 15:29:58 +0200

    proj start

--- src/App.js
+++ src/App.js
@@ -1,33 +1,33 @@
-import React, { Component } from "react";
-import ContactForm from "./components/ContactForm/ContactForm";
-import ContactList from "./components/ContactList/ContactList";
-import Filter from "./components/Filter/Filter";
-import s from "./App.module.css";
+import React, { Component } from 'react';
+import ContactForm from './components/ContactForm/ContactForm';
+import ContactList from './components/ContactList/ContactList';
+import Filter from './components/Filter/Filter';
+import s from './App.module.css';
 
 class App extends Component {
   state = {
     contacts: [
-      { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
-      { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
-      { id: "id-3", name: "Eden Clements", number: "645-17-79" },
-      { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+      { id: 'id-5', name: 'Ann Copel', number: '227-26' },
     ],
-    filter: "",
+    filter: '',
   };
 
-  formSubmitHandler = (user) => {
+  formSubmitHandler = user => {
     if (
-      this.state.contacts.find((el) => el.name.includes(user.name)) !==
-      undefined
+      this.state.contacts.find(el => el.name.includes(user.name)) !== undefined
     ) {
       alert(`${user.name} is already in contacts`);
     } else {
-      this.setState((prev) => ({
+      this.setState(prev => ({
         contacts: [...prev.contacts, user],
       }));
     }
   };
-  setFilter = (e) => {
+  setFilter = e => {
     const { value } = e.target;
     this.setState({
       filter: value,
@@ -35,13 +35,13 @@ class App extends Component {
   };
 
   filterInputHandler = () =>
-    this.state.contacts.filter((item) =>
+    this.state.contacts.filter(item =>
       item.name.toLowerCase().includes(this.state.filter.toLowerCase())
     );
 
-  removeUser = (id) => {
-    this.setState((prev) => ({
-      contacts: [...prev.contacts.filter((contact) => contact.id !== id)],
+  removeUser = id => {
+    this.setState(prev => ({
+      contacts: [...prev.contacts.filter(contact => contact.id !== id)],
     }));
   };
   render() {
