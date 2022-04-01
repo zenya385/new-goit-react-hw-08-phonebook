2280bc06513aa4a773a52b0a92c0b16ac1ae984f
Roman Zeno <zeno385@gmail.com>
2022-04-01 14:12:28 +0300

    fix

--- src/views/ContactsView.js
+++ src/views/ContactsView.js
@@ -1,29 +1,28 @@
 import React from 'react';
-import Contacts from "../components/contacts/Contacts";
-import Filter from "../components/filter/Filter";
-import DataInput from "../components/dataInput/DataInput";
-import Section from "../components/section/Section";
-
+import Contacts from '../components/contacts/Contacts';
+import Filter from '../components/filter/Filter.js';
+import DataInput from '../components/dataInput/DataInput';
+import Section from '../components/section/Section';
 
 const ContactsView = () => {
-    const style = {
-        app: {
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
-        }
-    }
-    return (
-        <div style={style.app}>
-            <Section title="Phonebook">
-                <DataInput/>
-            </Section>
-            <Section title="Contacts">
-                <Filter/>
-                <Contacts/>
-            </Section>
-        </div>
-    );
-}
+  const style = {
+    app: {
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+    },
+  };
+  return (
+    <div style={style.app}>
+      <Section title="Phonebook">
+        <DataInput />
+      </Section>
+      <Section title="Contacts">
+        <Filter />
+        <Contacts />
+      </Section>
+    </div>
+  );
+};
 
 export default ContactsView;
